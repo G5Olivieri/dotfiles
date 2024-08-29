4a5ffff77ac0c91ec04adddbbf5d413427a8120e
Glayson Olivieri <glayson@maggu.ai>
2024-08-29 10:33:26 -0300

    Add nerdfont install

---
+++ operation_systems/ubuntu/install-nerdfont.sh
@@ -0,0 +1,5 @@
+cd ~/Downloads
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+mkdir ~/.local/share/fonts
+tar -xvf -C ~/.local/share/fonts JetBrainsMono.tar.xz
+
